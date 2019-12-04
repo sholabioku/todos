e3d99114e6a65e255b8f553c5dfdc379fa9f26cb
sholabioku <bilushonline@gmail.com>
2019-12-04 20:03:41 +0100

    Case insensitive

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const generateTemplate = todo => {
 //Add todo
 addForm.addEventListener('submit', e => {
   e.preventDefault();
-  const todo = addForm.add.value.toLowerCase().trim();
+  const todo = addForm.add.value.trim();
   if (todo.length) {
     generateTemplate(todo);
     addForm.reset();
@@ -31,16 +31,16 @@ list.addEventListener('click', e => {
 
 const filterTodos = term => {
   Array.from(list.children)
-    .filter(todo => !todo.textContent.includes(term))
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
     .forEach(todo => todo.classList.add('filtered'));
   
     Array.from(list.children)
-    .filter(todo => todo.textContent.includes(term))
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
     .forEach(todo => todo.classList.remove('filtered'));
 };
 
 //Key up
 search.addEventListener('keyup', () => {
-  const term = search.value.trim();
+  const term = search.value.trim().toLowerCase();
   filterTodos(term);
 });
